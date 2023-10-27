476e8f8ab848edc8daced0f1cd6d30439074e94f
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2023-10-27 23:17:24 +0700

    new features - add a settings to change which site to arrive when clicked on a video - add a handle component for data loading and error

--- src/App.svelte
+++ src/App.svelte
@@ -3,27 +3,48 @@
 
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import Handle from './components/Handle.svelte';
 	import Card from './components/Card.svelte';
 
 	// Load data fetching function
 
 	import promise from './animatics';
+
+	// Choose which site to arrive when clicking on a video
+
+	let selected: string;
+
+	const sites = [
+		{ title: 'Youtube', url: 'https://youtube.com' },
+		{ title: 'Piped', url: 'https://piped.video' }
+	];
 </script>
 
 <!-- Header showing title, subtitle and setting -->
 <Header
 	title="OMORI Animatics"
-	desc="Collection of OMORI animatic videos on Youtube" />
+	desc="Collection of OMORI animatic videos on Youtube">
+	<br />
+	<label for="sites">Preferred website when clicking on a video: </label>
+	<select name="sites" id="sites" bind:value={selected}>
+		{#each sites as site}
+			<option value={site.url}>{site.title}</option>
+		{/each}
+	</select>
+</Header>
+
 <main>
 	{#await promise}
-		<h1>Loading</h1>
+		<!-- Show a loading message while loading (which doesn't happens long) -->
+		<Handle state="loading" />
 	{:then videos}
 		<!-- Lay out each video in a grid -->
 		{#each videos as video}
 			<Card data={video} site={selected} />
 		{/each}
 	{:catch error}
-		<h1>{error.message}</h1>
+		<!-- Show error message upon error (which never happens (hopefully)) -->
+		<Handle state="error" errorMSG={error.message} />
 	{/await}
 </main>
 
@@ -38,10 +59,16 @@
 		gap: 1rem;
 	}
 
-	@media (max-width: 500px) {
-		main {
-			padding: 0.6rem;
-			grid-template-columns: auto;
+	#sites {
+		background-color: inherit;
+		color: inherit;
+		border: solid 2px currentColor;
+		border-radius: 0;
+		font-family: inherit;
+		font-size: 1rem;
+
+		&:focus-visible {
+			outline: none;
 		}
 	}
 </style>
---
+++ src/components/Handle.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let state: 'loading' | 'error';
+	export let errorMSG: string = 'no error';
+</script>
+
+<div>
+	{#if state == 'loading'}
+		<h1>Loading...</h1>
+		<p>Getting data from Piped API</p>
+	{:else if state == 'error'}
+		<h1>Crashed!!!</h1>
+		<p>Error: {errorMSG}</p>
+	{/if}
+</div>
+
+<style>
+	div {
+		padding: 1rem;
+		background: black;
+		display: grid;
+		place-items: center;
+	}
+</style>
